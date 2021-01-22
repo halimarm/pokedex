186eb58a37776d68155aa9e8b788c0e680f61097
Halim Arumman <halimarumman@gmail.com>
2021-01-22 21:41:16 +0700

    add: routes

---
+++ src/pages/Detail/index.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react'
+
+const Detail: FC = () => {
+  return (
+    <h1>Detail</h1>
+  )
+}
+
+export default Detail
---
+++ src/routes/index.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react';
+import { Route } from 'react-router-dom';
+import Home from '../pages/Home';
+import Detail from '../pages/Detail';
+
+const Routes: FC = () => {
+  return (
+    <>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/:id" component={Detail} />
+    </>
+  );
+}
+
+export default Routes;
